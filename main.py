e943cff17876738e75e060a943d8824199982cb8
Nikita <basynin.nikita@ya.ru>
2022-06-16 13:04:53 +0300

    fixed script to work with Shodan API

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 import json
 
-API = ["LGY6W34GKPymODNywv863QEbz28AN5tz", "N89CVWcUZOiroWF4VOJXMiOkE8b21amk", "r4avkfUAFvJEYmHGoLZUnkOyYWJBRGqV"]
+API = ["bWeWmPvZDlNKU3SLMrIn2btImSZYwUeO"] #, "N89CVWcUZOiroWF4VOJXMiOkE8b21amk", "r4avkfUAFvJEYmHGoLZUnkOyYWJBRGqV"]
 
 
 def get_subdomains():
-    headers = {"apikey": API[0],
+    headers = {"apikey": API[-1], #fix 0
                "children_only": "false",
                "include_inactive": "false"}
     response = requests.get("https://api.securitytrails.com/v1/domain/spbu.ru/subdomains", headers=headers)
@@ -19,6 +19,8 @@ if __name__ == '__main__':
     subdomains = ''
     with open("m.txt", "r") as rf:
         subdomains = json.load(rf)
+    ips = open("ips.txt", "w")
+    adrs = open("adrs.txt", "w")
     for i, sd in enumerate(subdomains["subdomains"]):
         # sd = json.loads(response.text)["subdomains"][0]
         apikey = {"apikey": API[i%len(API)]}
@@ -30,6 +32,10 @@ if __name__ == '__main__':
             for j in range(len(data["current_dns"]["a"]["values"])):
                 a.append(data["current_dns"]["a"]["values"][j]["ip"])
             b = data["hostname"]
-            print(b, ":", a)
+            ip = str(str(b) + ":" )
+            for i in a:
+                ip = ip + i +" "
+                adrs.write(str(i + '\n'))
+            ips.write(ip + '\n')
         else:
-            print(data["hostname"])
+            ips.write(str(data["hostname"] + '\n'))
