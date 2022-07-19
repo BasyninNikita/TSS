8622759dea958471b08266b1fe9e21e6f9f6f535
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2022-07-19 12:17:45 +0300

    Create dependencies.py

---
+++ dependencies.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+if __name__ == "__main__":
+    langs = sys.argv[1].split(',')
+    #langs = 'python,golang,java'.split(',')
+    for lang in langs:
+        if lang.casefold() == 'python':
+            Out = os.popen('pip3 list').read()
+            print('%s deps: \n' %lang, Out)
+        elif lang.casefold() == 'java':
+            #os.popen('gradle init')
+            #Out = os.popen('gradle dependencies').read()
+            print("Java deps in process of development")
+        elif lang.casefold() == 'nodejs' or lang.casefold() == 'javascript':
+            Out = os.popen('npm list').read()
+            print('%s deps: \n' %lang, Out)
+        elif lang.casefold() == 'go':
+            Out = os.popen("go list -json ").read() 
+            print('%s deps: \n' %lang, Out)
+        else:
+            print('%s lang is unknown for me' %lang)
